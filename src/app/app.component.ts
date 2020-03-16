031cdfcaad74e93b0aa636edfa76aebc08352c58
Rayni Nuñez <timetopwm@gmail.com>
2020-03-15 20:40:23 -0400

    Primer commit, la gestion de productos esta completada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud';
+  title = 'Angular CRUD';
+  msg: string = '';
+  products = [
+    {id: 1, nombre: 'Platano', descripcion: 'maduro', fecha_venc: Date(), id_proveedor: 4}
+  ];
+
+  model: any = {};
+  model2: any = {};
+  hideUpdate: boolean = true;
+
+  addProduct(): void{
+    this.products.push(this.model);
+    this.msg = 'Producto agregado exitosamente';
+  }
+
+  deleteProduct(i): void{
+    var answer = confirm('Seguro que desea eliminar este producto?');
+    if(answer){
+      this.products.splice(i, 1);
+      this.msg = 'Producto borrado exitosamente'
+    }
+  }
+  myValue;
+
+  editProduct(i: number): void{
+    this.hideUpdate = false;
+    this.model2.id = this.products[i].id;
+    this.model2.nombre = this.products[i].nombre;
+    this.model2.descripcion = this.products[i].descripcion;
+    this.model2.fecha_venc = this.products[i].fecha_venc;
+    this.model2.id_proveedor = this.products[i].id_proveedor;
+    this.myValue = i;
+  }
+
+  updateProduct(): void{
+    let x = this.myValue;
+    let i = this.products.length;
+    console.log(i);
+    for(let j = 0; j <= i; j++){
+      //console.log(j);
+      if(j === x){
+        this.products[j] = this.model2;
+        this.msg = 'Producto actualizado exitosamente';
+        this.model2 = {};
+      }
+    }
+
+    
+  }
+
+  closeAlert(){
+    this.msg = '';
+  }
 }
